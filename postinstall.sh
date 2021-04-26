49c592a3040bf7c9fa7bd3f9bfc20be668c5407a
Mysy00 <pm@mysy.pl>
2021-04-26 19:03:49 +0200

    Update postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+timedatectl set-ntp true
 systemctl enable NetworkManager --now
 systemctl enable bluetooth --now
 setbg $HOME/.config/wall.jpg
+#pacman -S xf86-video-intel - necessary only on t450
